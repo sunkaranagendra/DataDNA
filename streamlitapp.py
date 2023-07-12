f9a3bc9ad98b473c3e56b34ab287603e4b6b08ca
sunkara nagendra kumar <56042167+sunkaranagendra@users.noreply.github.com>
2023-07-12 19:06:03 +0530

    Update streamlitapp.py
    creation-l2

--- streamlitapp.py
+++ streamlitapp.py
@@ -37,13 +37,13 @@ def main():
     st.header("Chat with csv by Pune Panthers")
     data_source=st.slidebar.selectbox("Data Source",["CSV","Database"])
     if data_source == "CSV":
-        uploaded_file=st.sidebar.file_uploader("Upload CSV file",type="csv")
-        user_question = st.text_input("Put your questions here: ")
-        if uploaded_file is not None and user_question is not None and user_question != "":
-            with st.spinner(text="In Progress..."):
-                df=pd.read_csv(uploaded_file)
-                agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
-                st.write(agent.run(user_question))
+      uploaded_file=st.sidebar.file_uploader("Upload CSV file",type="csv")
+      user_question = st.text_input("Put your questions here: ")
+      if uploaded_file is not None and user_question is not None and user_question != "":
+        with st.spinner(text="In Progress..."):
+          df=pd.read_csv(uploaded_file)
+          agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
+          st.write(agent.run(user_question))
     
     elif data_source== "Database":
         URI = get_URI()
@@ -51,10 +51,11 @@ def main():
             db = SQLDatabase.from_uri(URI)
             toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
             agent_executor = create_sql_agent(llm=OpenAI(temperature=0),toolkit=toolkit,verbose=True,agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
-      user_question = st.text_input("Put your questions here: ")
-      if user_question is not None and user_question != "":
-          with st.spinner(text="In Progress..."):
-              st.write(agent_db.run(user_question))
+        
+        user_question = st.text_input("Put your questions here: ")
+        if user_question is not None and user_question != "":
+            with st.spinner(text="In Progress..."):
+                st.write(agent_db.run(user_question))
         # db_host=st.sidebar.text_input("Database Host")
         # db_user=st.sidebar.text_input("Database User")
         # db_password=st.sidebar.text_input("Database Password")
