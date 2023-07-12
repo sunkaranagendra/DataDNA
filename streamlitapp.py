c82cab173584ee46a4c45ac2d111b17559b440b8
sunkara nagendra kumar <56042167+sunkaranagendra@users.noreply.github.com>
2023-07-12 10:36:08 +0530

    Update streamlitapp.py
    Creation-1

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,19 +1,24 @@
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+import streamlit as st
+import pandas as pd
+from langchain import Langchain
 
-Point = namedtuple('Point', 'x y')
-data = []
-
-points_per_turn = total_points / num_turns
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+langchain=Langchain()
+def main():
+    st.title("Data DNA Insights")
+    data_source=st.slidebar.selectbox("Data Source",["CSV","Database"])
+    if data_source == "CSV":
+        uploaded_file=st.sidebar.file_uploader("Upload CSV file",type="csv")
+        if uploaded_file is not None:
+            df=pd.read_csv(uploaded_file)
+            processed_data=langchain.process(df)
+            st.write(processed_data)
+    elif data_source== "Database":
+        db_host=st.sidebar.text_input("Database Host")
+        db_user=st.sidebar.text_input("Database User")
+        db_password=st.sidebar.text_input("Database Password")
+        db_name=st.sidebar.text_input("Database Name")
+        processed_data=langchain.process_from_db(host=db_host,user=db_user,password=db_password,db=db_name)
+        st.write(processed_data)
+        
+if __name__=="__main__":
+    main()
